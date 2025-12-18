74387278edca3f7f2f3db6a9ad91ba9e39741b6b
Wanderson Duarte Alves <wanderson.alves@grendene.com.br>
2025-12-18 10:50:59 -0300

    feat: criação do diretório class e da classe TabelaFrequencia

---
+++ class/tabelafrequencia.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+
+class TabelaFrequencia:
+
+    def regraDeSturges(self, n):
+        return int(np.ceil(1 + 3.3322 * np.log10(n)))
+
+    def amplitudeTotal(self, coluna): 
+        return coluna.max() - coluna.min()
+    
+    def amplitudeDeClasse(self, amplitudetotal, qtdclasses):
+        return max(1, int(np.round(amplitudetotal / qtdclasses)))
+    
+    def gerarBinsLabels(self, lista, qtdClasse):
+        amplitude_total = self.amplitudeTotal(lista)
+        amplitude_classe = self.amplitudeDeClasse(amplitude_total, qtdClasse)
+
+        valor = lista.min()
+        bins = [valor]
+        labels = []
+
+        for i in range(qtdClasse):
+            inicio = valor
+            valor += amplitude_classe
+            bins.append(valor)
+            labels.append(f'{inicio}-{valor}')
+
+        bin[-1] = lista.max()
+
+        return bins, labels
+    
+    @staticmethod
+    def dadosAgrupadosPorClasse(self, data, colunaNome):
+        lista = data[colunaNome].copy().sort_values()
+        regrasturges = self.regraDeSturges(lista.count())
+        bins, labels = self.gerarBinsLabels(lista, regrasturges)
+        newdata = data.copy()
+        newdata['Agrupado'] = pd.cut(data[colunaNome], bins= bins, labels= labels)
+        return newdata
